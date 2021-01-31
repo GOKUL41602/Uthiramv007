0cd8a726739c7b15c99dbe69d3a3ad7a6949ed3a
Gokul <gokul.18it@kct.ac.in>
2021-01-31 14:31:45 +0530

    Tenth Commit-30/01/2021

--- app/src/main/java/com/uthiram/uthiramv007/LoginPage.java
+++ app/src/main/java/com/uthiram/uthiramv007/LoginPage.java
@@ -1,17 +1,24 @@
 package com.uthiram.uthiramv007;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -25,6 +32,8 @@ public class LoginPage extends AppCompatActivity {
 
     private DatabaseReference reference;
 
+    private RelativeLayout relativeLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +46,7 @@ public class LoginPage extends AppCompatActivity {
                 initializeStrings();
                 if (validateUserName()) {
                     if (validatePassword()) {
-                        Toast.makeText(LoginPage.this, "validation Successful", Toast.LENGTH_SHORT).show();
+                        verifyUserId();
                     } else {
                         validatePassword();
                     }
@@ -74,7 +83,31 @@ public class LoginPage extends AppCompatActivity {
     }
 
     private void verifyUserId() {
-        reference=FirebaseDatabase.getInstance().getReference("DonorsDto");
+        reference = FirebaseDatabase.getInstance().getReference("DonorsDto");
+        Query query = reference.orderByChild("rollNo").startAt(userNameText).endAt(userNameText + "\uf8ff");
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    passwordFromDB = snapshot.child(userNameText).child("password").getValue(String.class);
+                    if (passwordText.equals(passwordFromDB)) {
+                        Intent intent = new Intent(LoginPage.this, DonorHomePage.class);
+                        startActivity(intent);
+                    } else {
+                        password.setError("Incorrect Password");
+                        password.requestFocus();
+                    }
+                } else {
+                    userName.setError("UserName doesn't Exists");
+                    userName.requestFocus();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
     }
 
@@ -97,6 +130,8 @@ public class LoginPage extends AppCompatActivity {
         loginBtn = findViewById(R.id.loginPage_loginBtn);
         cancelBtn = findViewById(R.id.loginPage_cancelBtn);
         registerBtn = findViewById(R.id.loginPage_registerBtn);
+
+        relativeLayout = findViewById(R.id.loginPage_relLayout);
 
         reference = FirebaseDatabase.getInstance().getReference("DonorsDto");
     }
