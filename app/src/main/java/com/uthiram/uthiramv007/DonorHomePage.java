4a3c8d0b1579e7a7297edc27bc3d0c30c7dae1cb
Gokul <gokul.18it@kct.ac.in>
2021-01-31 13:43:44 +0530

    Ninth Commit-30/01/2021

---
+++ app/src/main/java/com/uthiram/uthiramv007/DonorHomePage.java
@@ -0,0 +1,14 @@
+package com.uthiram.uthiramv007;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DonorHomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donor_home_page);
+    }
+}
