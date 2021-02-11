a260889c3d85197a77cebf09a6c7c58e0cc76768
Gokul <gokul.18it@kct.ac.in>
2021-02-11 16:12:54 +0530

    Twenty-Five Implemented Navigation Drawer in Donor Home Page but not working!

--- app/src/main/java/com/uthiram/uthiramv007/DonorHomePage.java
+++ app/src/main/java/com/uthiram/uthiramv007/DonorHomePage.java
@@ -1,17 +1,29 @@
 package com.uthiram.uthiramv007;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class DonorHomePage extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+import static com.uthiram.uthiramv007.R.string.navigation_draw_open;
+
+public class DonorHomePage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
 
     private TextView title;
 
@@ -28,6 +40,22 @@ public class DonorHomePage extends AppCompatActivity {
 
         userName = getIntent().getStringExtra("userName");
         title.setText("Welcome " + userName);
+
+
+        drawerLayout = findViewById(R.id.design_navigation_view);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, navigation_draw_open, R.string.navigation_draw_close);
+
+        drawerLayout.addDrawerListener(toggle);
+
+        toggle.syncState();
+
 
         viewProfileBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,4 +114,36 @@ public class DonorHomePage extends AppCompatActivity {
         logOutBtn = findViewById(R.id.donorHomePage_logOutBtn);
     }
 
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.edit_donor_profile:Intent intent1 = new Intent(DonorHomePage.this, ViewDonorProfile.class);
+                intent1.putExtra("userName", userName);
+                startActivity(intent1);
+                break;
+            case R.id.edit_donor_status:
+                Intent intent2 = new Intent(DonorHomePage.this, UpdateDonorDetailsPage.class);
+                intent2.putExtra("userName", userName);
+                startActivity(intent2);
+                break;
+            case R.id.donor_logout:
+                Intent intent3 = new Intent(DonorHomePage.this, LoginPage.class);
+                startActivity(intent3);
+                DonorHomePage.this.finish();
+                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
 }
