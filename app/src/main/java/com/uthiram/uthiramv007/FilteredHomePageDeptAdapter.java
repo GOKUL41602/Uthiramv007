3cd8ef5119db3d467e7f7459d993e6ff2f93403b
Gokul41602 <gokul.18it@kct.ac.in>
2021-02-26 13:55:19 +0700

    Eight-Three
    completely added progress bar in all filter search pages

--- app/src/main/java/com/uthiram/uthiramv007/FilteredHomePageDeptAdapter.java
+++ app/src/main/java/com/uthiram/uthiramv007/FilteredHomePageDeptAdapter.java
@@ -1,10 +1,8 @@
 package com.uthiram.uthiramv007;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
